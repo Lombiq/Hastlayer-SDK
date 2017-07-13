2575f2017f0388b3263ce6c72f3f65b3e2280c6a
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2017-07-13 16:13:27 +0200

    Moving serial port configurator to Hast.Xilinx.Abstractions because it's also needed in the Client flavor
    --HG--
    branch : issue/HAST-118

---
+++ Hast.Abstractions/Hast.Xilinx.Abstractions/SerialPortConfigurator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Hast.Common.Extensibility.Pipeline;
+using Hast.Communication.Extensibility.Pipeline;
+using Hast.Communication.Models;
+
+namespace Hast.Xilinx.Abstractions
+{
+    public class SerialPortConfigurator : PipelineStepBase, ISerialPortConfigurator
+    {
+        public void ConfigureSerialPort(SerialPort serialPort, IHardwareExecutionContext hardwareExecutionContext)
+        {
+            if (hardwareExecutionContext.HardwareRepresentation.DeviceManifest.Name != Nexys4DdrManifestProvider.DeviceName) return;
+
+            serialPort.BaudRate = 230400;
+        }
+    }
+}
