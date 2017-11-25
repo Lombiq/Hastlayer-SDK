b03a1020ac4a2ac439cf3d494940bfc11cfe774f
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2017-11-25 19:24:10 +0100

    Removing debug files
    --HG--
    branch : issue/HAST-97

--- Samples/Hast.Samples.Kpz/CopyVHDL.bat
+++
@@ -1,4 +0,0 @@
-dir bin\Debug\Hast_IP_proc.vhd
-copy bin\Debug\Hast_IP_proc.vhd ..\..\..\hastlayer-hardware\IPRepo\Hast_IP.vhd
-rem copy bin\Debug\Hast_IP.vhd ..\..\..\hastlayer-hardware\IPRepo
-pause 
