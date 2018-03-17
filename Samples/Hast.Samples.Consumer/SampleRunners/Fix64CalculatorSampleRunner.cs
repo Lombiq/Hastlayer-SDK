c93daf5dd986b6c29bdf034652b3766672ceda34
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2018-03-17 02:56:25 +0100

    Removing empty loop from Fix64Calculator and adding software benchmark
    --HG--
    branch : dev

--- Samples/Hast.Samples.Consumer/SampleRunners/Fix64CalculatorSampleRunner.cs
+++ Samples/Hast.Samples.Consumer/SampleRunners/Fix64CalculatorSampleRunner.cs
@@ -13,17 +13,18 @@ namespace Hast.Samples.Consumer.SampleRunners
     {
         public static void Configure(HardwareGenerationConfiguration configuration)
         {
+            RunSoftwareBenchmark();
             configuration.AddHardwareEntryPointType<Fix64Calculator>();
         }
 
         public static async Task Run(IHastlayer hastlayer, IHardwareRepresentation hardwareRepresentation)
         {
-            var fixed64Showcase = await hastlayer.GenerateProxy(hardwareRepresentation, new Fix64Calculator());
+            var fixed64Calculator = await hastlayer.GenerateProxy(hardwareRepresentation, new Fix64Calculator());
 
-            var sum = fixed64Showcase.CalculateIntegerSumUpToNumber(10000000);
+            var sum = fixed64Calculator.CalculateIntegerSumUpToNumber(10000000);
 
-            // This takes about 264ms on an i7 processor with 4 physical (8 logical) cores and 1300ms on an FPGA (with 
-            // a MaxDegreeOfParallelism of 10 while the device is about 51% utilized). With a higher degree of 
+            // This takes about 252ms on an i7 processor with 4 physical (8 logical) cores and 1300ms on an FPGA (with 
+            // a MaxDegreeOfParallelism of 10 while the device is about 58% utilized). With a higher degree of 
             // parallelism it won't fit on the Nexys 4 DDR board's FPGA.
             // Since this basically does what the single-threaded sample but in multiple copies on multiple threads
             // the single-threaded sample takes the same amount of time on the FPGA.
@@ -31,11 +32,28 @@ namespace Hast.Samples.Consumer.SampleRunners
             // Creating an array of numbers alternating between 9999999 and 10000001 so we can also see that threads
             // don't step on each other's feet.
             var numbers = new int[Fix64Calculator.MaxDegreeOfParallelism];
-            for (int i = 1; i < Fix64Calculator.MaxDegreeOfParallelism; i++)
+            for (int i = 0; i < Fix64Calculator.MaxDegreeOfParallelism; i++)
             {
                 numbers[i] = 10000000 + (i % 2 == 0 ? -1 : 1); 
             }
-            var sums = fixed64Showcase.ParallelizedCalculateIntegerSumUpToNumbers(numbers);
+            var sums = fixed64Calculator.ParallelizedCalculateIntegerSumUpToNumbers(numbers);
+        }
+
+        public static void RunSoftwareBenchmark()
+        {
+            var fixed64Calculator = new Fix64Calculator();
+
+            var numbers = new int[Fix64Calculator.MaxDegreeOfParallelism];
+            for (int i = 0; i < Fix64Calculator.MaxDegreeOfParallelism; i++)
+            {
+                numbers[i] = 10000000 + (i % 2 == 0 ? -1 : 1);
+            }
+            var sums = fixed64Calculator.ParallelizedCalculateIntegerSumUpToNumbers(numbers);
+
+            var sw = System.Diagnostics.Stopwatch.StartNew();
+            sums = fixed64Calculator.ParallelizedCalculateIntegerSumUpToNumbers(numbers);
+            sw.Stop();
+            Console.WriteLine("Elapsed ms: " + sw.ElapsedMilliseconds);
         }
     }
 }
