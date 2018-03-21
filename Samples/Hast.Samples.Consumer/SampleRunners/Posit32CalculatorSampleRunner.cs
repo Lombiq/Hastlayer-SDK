cd3abb96e139ab5f5ece91c69cc6b8a7cdc35ee7
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2018-03-21 01:23:52 +0100

    Making Posit32 software benchmarks print results a bit more loosely and adding CalculatePowerOfReal() hardware execution
    --HG--
    branch : issue/HAST-136

--- Samples/Hast.Samples.Consumer/SampleRunners/Posit32CalculatorSampleRunner.cs
+++ Samples/Hast.Samples.Consumer/SampleRunners/Posit32CalculatorSampleRunner.cs
@@ -27,6 +27,9 @@ namespace Hast.Samples.Consumer.SampleRunners
             var integerSumUpToNumber = positCalculator.CalculateIntegerSumUpToNumber(100000);
 
 
+            positCalculator.CalculatePowerOfReal(100000, (float)1.0001);
+
+
             var numbers = new int[Posit32Calculator.MaxDegreeOfParallelism];
             for (int i = 0; i < Posit32Calculator.MaxDegreeOfParallelism; i++)
             {
@@ -62,6 +65,16 @@ namespace Hast.Samples.Consumer.SampleRunners
             Console.WriteLine("Result of counting up to 100000: " + integerSumUpToNumber);
             Console.WriteLine("Elapsed: " + sw.ElapsedMilliseconds + "ms");
 
+            Console.WriteLine();
+
+            sw = Stopwatch.StartNew();
+            var powerOfReal = positCalculator.CalculatePowerOfReal(100000, (float)1.0001);
+            sw.Stop();
+
+            Console.WriteLine("Result of power of real number: " + powerOfReal);
+            Console.WriteLine("Elapsed: " + sw.ElapsedMilliseconds + "ms");
+
+            Console.WriteLine();
 
             var numbers = new int[Posit32Calculator.MaxDegreeOfParallelism];
             for (int i = 0; i < Posit32Calculator.MaxDegreeOfParallelism; i++)
@@ -78,6 +91,7 @@ namespace Hast.Samples.Consumer.SampleRunners
             Console.WriteLine("Result of counting up to ~100000 parallelized: " + string.Join(", ", integerSumsUpToNumbers));
             Console.WriteLine("Elapsed: " + sw.ElapsedMilliseconds + "ms");
 
+            Console.WriteLine();
 
             var posit32Array = new uint[100000];
 
@@ -94,12 +108,7 @@ namespace Hast.Samples.Consumer.SampleRunners
             Console.WriteLine("Result of addition of posits in array: " + positsInArraySum);
             Console.WriteLine("Elapsed: " + sw.ElapsedMilliseconds + "ms");
 
-            sw = Stopwatch.StartNew();
-            var powerOfReal = positCalculator.CalculatePowerOfReal(100000,(float) 1.0001);
-            sw.Stop();
-
-            Console.WriteLine("Result of power of real number: " + powerOfReal);
-            Console.WriteLine("Elapsed: " + sw.ElapsedMilliseconds + "ms");
+            Console.WriteLine();
         }
     }
 }
