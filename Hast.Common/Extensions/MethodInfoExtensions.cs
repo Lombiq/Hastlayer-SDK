dcaaba5a33a4fe5df542fc45d6761cd0aea1693c
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2018-05-11 23:32:23 +0200

    Code styling
    --HG--
    branch : dev

--- Hast.Common/Extensions/MethodInfoExtensions.cs
+++ Hast.Common/Extensions/MethodInfoExtensions.cs
@@ -8,13 +8,10 @@ namespace System.Reflection
         /// Gets the full name of the method, including the full namespace of the parent type(s) as well as their return 
         /// type and the types of their (type) arguments.
         /// </summary>
-        public static string GetFullName(this MethodInfo method)
-        {
-            return
-                method.ReturnType.FullName + " " +
-                method.ReflectedType.FullName + "::" +
-                method.Name + 
-                "(" + string.Join(",", method.GetParameters().Select(parameter => parameter.ParameterType.FullName)) + ")";
-        }
+        public static string GetFullName(this MethodInfo method) =>
+            method.ReturnType.FullName + " " +
+            method.ReflectedType.FullName + "::" +
+            method.Name + 
+            "(" + string.Join(",", method.GetParameters().Select(parameter => parameter.ParameterType.FullName)) + ")";
     }
 }
