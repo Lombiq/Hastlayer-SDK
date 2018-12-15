9df4210e860c75c6fe89799094969ccdaba7ea92
David El-Saig <daud.ici@gmail.com>
2018-12-15 07:16:36 -0800

    Extracted CatapultLibrary intergace.
    --HG--
    branch : issue/HAST-149

---
+++ Hast.Abstractions/Hast.Catapult.Abstractions/ICatapultLibrary.cs
@@ -0,0 +1,20 @@
+﻿using Orchard;
+using System;
+using System.Threading.Tasks;
+
+namespace Hast.Catapult.Abstractions
+{
+    public interface ICatapultLibrary : IDisposable, ISingletonDependency
+    {
+        int BufferCount { get; }
+        int BufferSize { get; }
+        IntPtr Handle { get; }
+        CatapultLogFunction LogFunction { get; }
+        ICatapultNativeLibrary NativeLibrary { get; }
+        bool PcieEnabled { get; }
+        int ShellRegisterCount { get; }
+
+        Task<Memory<byte>> ExecuteJob(int bufferIndex, Memory<byte> inputData);
+        void VerifyResult(Constants.Status status);
+    }
+}
