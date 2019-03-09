d8186809eafb57454312a438ebc7ffe221e5cb8c
David El-Saig <david.el-saig@lombiq.com>
2019-03-09 15:29:59 -0800

    Code organization and bug fix.
    --HG--
    branch : issue/HAST-150

---
+++ Hast.Communication.Tester/Options.cs
@@ -0,0 +1,46 @@
+﻿using CommandLine;
+using Hast.Transformer.Abstractions.SimpleMemory;
+using System;
+
+namespace Hast.Communication.Tester
+{
+    public class Options
+    {
+        [Option('l', "list", HelpText = "List available devices and exit.")]
+        public bool ListDevices { get; set; }
+
+        [Option('d', "device", HelpText = "Name of the selected device.")]
+        public string DeviceName { get; set; }
+
+        [Option('b', "bytes", HelpText = "The total size of the payload in bytes.")]
+        public long PayloadBytes { get; set; } = 10;
+
+        [Option('k', "kilo-bytes", HelpText = "The total size of the payload in kilobytes.")]
+        public int PayloadKiloBytes { get => (int)(PayloadBytes / 1024); set => PayloadBytes = (long)value * 1024; }
+
+        [Option('c', "cells", HelpText = "The total size of the payload in number of cells.")]
+        public int PayloadLengthCells
+        {
+            get => (int)Math.Ceiling((double)PayloadBytes / SimpleMemory.MemoryCellSizeBytes);
+            set => PayloadBytes = (long)value * SimpleMemory.MemoryCellSizeBytes;
+        }
+
+        [Option('m', "member-id", HelpText = "The simulated MemberId.")]
+        public int MemberId { get; set; } = 1;
+
+        [Option('t', "payload-type", HelpText = "What kind of data to send (ConstantIntOne, Counter, Random, BinaryFile)")]
+        public PayloadType PayloadType { get; set; } = PayloadType.ConstantIntOne;
+
+        [Option('f', "file-type", HelpText = "Type of the files where input and output are dumped to(None, Hexdump, Binary)")]
+        public OutputFileType OutputFileType { get; set; } = OutputFileType.None;
+
+        [Option('i', "input", HelpText = "Generated data is saved to or payload is read from here when using BinaryFile as file-type.")]
+        public string InputFileName { get; set; }
+
+        [Option('o', "output", HelpText = "Output file name. (overrides -f to Hexdump if it's None; use value '-' to write Hexdump to the console)")]
+        public string OutputFileName { get; set; }
+
+        [Option('j', "json", HelpText = "Create a summary as JSON file.")]
+        public string JsonOutputFileName { get; set; }
+    }
+}
--- Hast.Communication.Tester/Program.cs
+++ Hast.Communication.Tester/Program.cs
@@ -13,89 +13,9 @@ using System.Threading.Tasks;
 
 namespace Hast.Communication.Tester
 {
-    class Program
+    partial class Program
     {
-        public enum PayloadType
-        {
-            /// <summary>
-            /// Each cell contains an int value of 1 (00000001h)
-            /// </summary>
-            ConstantIntOne,
-
-            /// <summary>
-            /// Each cell had 1 larger value than the previous one, overflow is permitted.
-            /// </summary>
-            Counter,
-
-            /// <summary>
-            /// Each cell gets a random value.
-            /// </summary>
-            Random,
-
-            /// <summary>
-            /// File gets read as binary.
-            /// </summary>
-            BinaryFile
-        }
-
-        public enum OutputFileType
-        {
-            /// <summary>
-            /// No output file is to be generated.
-            /// </summary>
-            None,
-
-            /// <summary>
-            /// The output is saved as a text file containing a sequence of hexadecimal numbers in 8 digit groups.
-            /// </summary>
-            Hexdump,
-
-            /// <summary>
-            /// The output is saved as raw binary file.
-            /// </summary>
-            Binary
-        }
-
-        public class Options
-        {
-            [Option('l', "list", HelpText = "List available devices and exit.")]
-            public bool ListDevices { get; set; }
-
-            [Option('d', "device", HelpText = "Name of the selected device.")]
-            public string DeviceName { get; set; }
-
-            [Option('b', "bytes", HelpText = "The total size of the payload in bytes.")]
-            public long PayloadBytes { get; set; } = 10;
-
-            [Option('k', "kilo-bytes", HelpText = "The total size of the payload in kilobytes.")]
-            public int PayloadKiloBytes { get => (int)(PayloadBytes / 1024); set => PayloadBytes = (long)value * 1024; }
-
-            [Option('c', "cells", HelpText = "The total size of the payload in number of cells.")]
-            public int PayloadLengthCells
-            {
-                get => (int)Math.Ceiling((double)PayloadBytes / SimpleMemory.MemoryCellSizeBytes);
-                set => PayloadBytes = (long)value * SimpleMemory.MemoryCellSizeBytes;
-            }
-
-            [Option('m', "member-id", HelpText = "The simulated MemberId.")]
-            public int MemberId { get; set; } = 1;
-
-            [Option('t', "payload-type", HelpText = "What kind of data to send (ConstantIntOne, Counter, Random, BinaryFile)")]
-            public PayloadType PayloadType { get; set; } = PayloadType.ConstantIntOne;
-
-            [Option('f', "file-type", HelpText = "Type of the files where input and output are dumped to(None, Hexdump, Binary)")]
-            public OutputFileType OutputFileType { get; set; } = OutputFileType.None;
-
-            [Option('i', "input", HelpText = "Generated data is saved to or payload is read from here when using BinaryFile as file-type.")]
-            public string InputFileName { get; set; }
-
-            [Option('o', "output", HelpText = "Output file name. (overrides -f to Hexdump if it's None; use value '-' to write Hexdump to the console)")]
-            public string OutputFileName { get; set; }
-
-            [Option('j', "json", HelpText = "Create a summary as JSON file.")]
-            public string JsonOutputFileName { get; set; }
-        }
-
+        
         public const string DefaultHexdumpFileName = "dump.txt";
         public const string DefaultBinaryFileName = "dump.bin";
 
@@ -157,8 +77,9 @@ namespace Hast.Communication.Tester
                     case PayloadType.BinaryFile:
                         using (var fileStream = File.OpenRead(configuration.InputFileName))
                         {
-                            var data = new byte[fileStream.Length];
-                            fileStream.Read(data, 3 * SimpleMemory.MemoryCellSizeBytes, data.Length);
+                            int prefixBytes = 3 * SimpleMemory.MemoryCellSizeBytes;
+                            var data = new byte[fileStream.Length + prefixBytes];
+                            fileStream.Read(data, prefixBytes, (int)fileStream.Length);
                             accessor.Set(data, 3);
                         }
                         break;
