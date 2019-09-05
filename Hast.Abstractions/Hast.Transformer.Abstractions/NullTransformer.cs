f6dc5e95624ce586198ee9026c472fb6461ce1ac
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2019-09-05 22:44:17 +0200

    Adding NullTransformer to handle the lack of an ITransformer implementation more gracefully
    --HG--
    branch : issue/HAST-131

---
+++ Hast.Abstractions/Hast.Transformer.Abstractions/NullTransformer.cs
@@ -0,0 +1,59 @@
+﻿using Hast.Layer;
+using Orchard.Logging;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hast.Transformer.Abstractions
+{
+    public class NullTransformer : ITransformer
+    {
+        public ILogger Logger { get; set; }
+
+
+        public NullTransformer()
+        {
+            Logger = NullLogger.Instance;
+        }
+
+
+        public Task<IHardwareDescription> Transform(IEnumerable<string> assemblyPaths, IHardwareGenerationConfiguration configuration)
+        {
+            Logger.Warning("No Transformer is available. This most possibly means an issue.");
+
+            // If you hit this during debugging then it's a problem, there should be a proper ITransformer 
+            // implementation.
+            Debugger.Break();
+
+            var mockHardwareEntryPointMappings = new Dictionary<string, int>();
+
+            for (int i = 0; i < configuration.HardwareEntryPointMemberFullNames.Count; i++)
+            {
+                mockHardwareEntryPointMappings[configuration.HardwareEntryPointMemberFullNames[i]] = i;
+            }
+
+            return Task.FromResult<IHardwareDescription>(new HardwareDescription(mockHardwareEntryPointMappings));
+        }
+
+
+        private class HardwareDescription : IHardwareDescription
+        {
+            public string Language => "VHDL";
+
+            public IReadOnlyDictionary<string, int> HardwareEntryPointNamesToMemberIdMappings { get; }
+
+            public IEnumerable<ITransformationWarning> Warnings => Enumerable.Empty<ITransformationWarning>();
+
+
+            public HardwareDescription(IReadOnlyDictionary<string, int> hardwareEntryPointNamesToMemberIdMappings)
+            {
+                HardwareEntryPointNamesToMemberIdMappings = hardwareEntryPointNamesToMemberIdMappings;
+            }
+
+
+            public Task WriteSource(Stream stream) => Task.CompletedTask;
+        }
+    }
+}
