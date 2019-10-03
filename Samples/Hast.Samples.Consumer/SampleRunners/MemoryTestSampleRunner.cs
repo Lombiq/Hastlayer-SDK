d6fa6e109336845d5fbf85a6104ca21eeef79bad
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2019-10-03 17:00:25 +0200

    Adding more MemoryTest runs
    --HG--
    branch : issue/HAST-131

--- Samples/Hast.Samples.Consumer/SampleRunners/MemoryTestSampleRunner.cs
+++ Samples/Hast.Samples.Consumer/SampleRunners/MemoryTestSampleRunner.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Hast.Layer;
+﻿using Hast.Layer;
 using Hast.Samples.SampleAssembly;
+using System.Threading.Tasks;
 
 namespace Hast.Samples.Consumer.SampleRunners
 {
@@ -19,9 +15,13 @@ namespace Hast.Samples.Consumer.SampleRunners
         {
             var memoryTest = await hastlayer.GenerateProxy(hardwareRepresentation, new MemoryTest());
 
-            var output1 = memoryTest.Run(3, 7);
-            var output2 = memoryTest.Run(0, 50);
-            var output3 = memoryTest.Run(47, 100);
+            var output1 = memoryTest.Run(0, 1);
+            var output2 = memoryTest.Run(0, 3);
+            var output3 = memoryTest.Run(0, 7);
+            var output4 = memoryTest.Run(0, 50);
+            var output5 = memoryTest.Run(1, 1);
+            var output6 = memoryTest.Run(3, 7);
+            var output7 = memoryTest.Run(47, 100);
         }
     }
 }
