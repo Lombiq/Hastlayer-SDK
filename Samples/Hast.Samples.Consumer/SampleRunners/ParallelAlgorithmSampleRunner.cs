cde84bdda55a6d1cfa6e9757baca0409b5751fc2
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2019-10-14 20:26:34 +0200

    Adding notes on the PC execution of ParallelAlgorithm with a parallelization of 700
    --HG--
    branch : issue/HAST-131

--- Samples/Hast.Samples.Consumer/SampleRunners/ParallelAlgorithmSampleRunner.cs
+++ Samples/Hast.Samples.Consumer/SampleRunners/ParallelAlgorithmSampleRunner.cs
@@ -26,9 +26,10 @@ namespace Hast.Samples.Consumer.SampleRunners
             // This takes about 1900ms on an i7 processor with 4 physical (8 logical) cores and 300ms on an FPGA (with 
             // a MaxDegreeOfParallelism of 280 while the device is about 80% utilized). With a higher degree of 
             // parallelism it won't fit on the Nexys 4 DDR board's FPGA.
-            // On Catapult a MaxDegreeOfParallelism of 700 will fit as well (70% resource utilization) and run in about 
-            // 200ms (including communication latency). Compiling that hardware design will take about 14.5 hours 
-            // though (with MaxDegreeOfParallelism 600 it'll take about 4).
+            // On Catapult a MaxDegreeOfParallelism of 700 will fit as well (70% resource utilization) and run in about
+            // 200ms (including communication latency) vs about 5s on the previous reference PC. Compiling that
+            // hardware design will take about 14.5 hours though (with MaxDegreeOfParallelism of 600 it'll take about
+            // 4).
             var output1 = parallelAlgorithm.Run(234234);
             var output2 = parallelAlgorithm.Run(123);
             var output3 = parallelAlgorithm.Run(9999);
