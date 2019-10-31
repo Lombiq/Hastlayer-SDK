fc25feb99b898b5d318344f1057665292661c011
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2019-10-31 01:31:21 +0100

    Using abbreviations in and adding simplifcations to the F# sample
    --HG--
    branch : issue/HAST-131

--- Hast.Samples.FSharpSampleAssembly/FSharpParallelAlgorithmContainer.fs
+++ Hast.Samples.FSharpSampleAssembly/FSharpParallelAlgorithmContainer.fs
@@ -3,7 +3,6 @@
 module FSharpParallelAlgorithmContainer =
     open Hast.Transformer.Abstractions.SimpleMemory
     open System.Threading.Tasks
-    open System
 
     // A simple parallelized sample showcasing what you can also see in ParallelAlgorithm, but in F#.
     // Due to how Hastlayer processes the .NET assemblies unfortunately this has to be very similar to the C# sample,
@@ -24,24 +23,24 @@ module FSharpParallelAlgorithmContainer =
         // Since there's no virtual in F# the hardware entry point methods need to be abstracts with the default
         // implementation following them.
         abstract member Run: SimpleMemory -> unit
-        default this.Run memory = 
+        default __.Run memory = 
             let input = memory.ReadInt32(Run_InputUInt32Index)
             // You need to use Array.zeroCreate when creating an empty array for Hastlayer to understand it.
-            let tasks : Task<int> array = Array.zeroCreate MaxDegreeOfParallelism
+            let tasks = Array.zeroCreate MaxDegreeOfParallelism
 
             // Since the for loop uses an inclusive upper bound we use a while loop here for clarity with the C# 
             // ParallelAlgorithm sample.
             let mutable i = 0
             while i < MaxDegreeOfParallelism do
-                tasks.[i] <- Task.Factory.StartNew((fun (indexObject : Object) -> 
-                    let index : int = indexObject :?> int
+                tasks.[i] <- Task.Factory.StartNew((fun (indexObject : obj) -> 
+                    let index = indexObject :?> int
                     let mutable result = input + index * 2
 
                     let mutable even = true
                     for j = 2 to 9999998 do
                         if even then result <- result + index
                         else result <- result - index
-                        even <- not(even)
+                        even <- not even
 
                     result), i)
                 i <- i + 1
@@ -56,7 +55,7 @@ module FSharpParallelAlgorithmContainer =
 
             memory.WriteInt32(Run_InputUInt32Index, output)
 
-        member this.Run input : int =
+        member this.Run input =
             let memory = new SimpleMemory(1)
             memory.WriteInt32(Run_InputUInt32Index, input)
             this.Run memory
