7661cec7e64800f28fa46ae96a2da7a02c09400e
David El-Saig <david.el-saig@lombiq.com>
2020-02-20 22:17:22 +0100

    Removed IUnitOfWorkDependency as it's not supported by Core DI.
    --HG--
    branch : issue/HAST-156

--- Hast.Common/Interfaces/IDependency.cs
+++ Hast.Common/Interfaces/IDependency.cs
@@ -19,31 +19,9 @@ namespace Hast.Common.Interfaces
     }
 
     /// <summary>
-    /// Base interface for services that may *only* be instantiated in a unit of work.
-    /// This interface is used to guarantee they are not accidentally referenced by a singleton dependency.
-    /// </summary>
-    public interface IUnitOfWorkDependency : IDependency
-    {
-    }
-
-    /// <summary>
     /// Base interface for services that are instantiated per usage.
     /// </summary>
     public interface ITransientDependency : IDependency
     {
-    }
-
-
-    public abstract class Component : IDependency
-    {
-        protected Component()
-        {
-            throw new NotImplementedException();
-            //Logger = NullLogger.Instance;
-            //T = NullLocalizer.Instance;
-        }
-
-        //public ILogger Logger { get; set; }
-        //public Localizer T { get; set; }
     }
 }
