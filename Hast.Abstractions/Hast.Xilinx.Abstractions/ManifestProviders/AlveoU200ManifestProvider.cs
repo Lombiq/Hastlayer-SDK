84dc1b5104f61bbd3b75c797516602012d311e13
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2020-03-20 19:47:37 +0100

    Fixing comment in the U200 and U250 manifest providers
    --HG--
    branch : issue/HAST-157

--- Hast.Abstractions/Hast.Xilinx.Abstractions/ManifestProviders/AlveoU200ManifestProvider.cs
+++ Hast.Abstractions/Hast.Xilinx.Abstractions/ManifestProviders/AlveoU200ManifestProvider.cs
@@ -13,8 +13,8 @@ namespace Hast.Xilinx.Abstractions.ManifestProviders
                 Name = DeviceName,
                 ClockFrequencyHz = 300000000, // 300 Mhz
                 SupportedCommunicationChannelNames = new[] { "SDAccel" },
-                // While there is 8GB of HBM2 and 32GB DDR RAM the max object size in .NET is 2GB. So until we
-                // add paging to SimpleMemory the limit is 2GB, see: https://github.com/Lombiq/Hastlayer-SDK/issues/27
+                // While there is 64GB DDR RAM the max object size in .NET is 2GB. So until we add paging to
+                // SimpleMemory the limit is 2GB, see: https://github.com/Lombiq/Hastlayer-SDK/issues/27
                 AvailableMemoryBytes = 2_000_000_000UL,
                 ToolChainName = CommonToolChainNames.Vivado
             };
