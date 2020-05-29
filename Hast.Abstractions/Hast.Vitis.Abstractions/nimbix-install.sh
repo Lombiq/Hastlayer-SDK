1af1fb845df5f0ea1a7eb5171516dad62a97dbb9
Dávid El-Saig <david.el-saig@lombiq.com>
2020-05-29 04:49:50 +0200

    Bug fix.

--- Hast.Abstractions/Hast.Vitis.Abstractions/nimbix-install.sh
+++ Hast.Abstractions/Hast.Vitis.Abstractions/nimbix-install.sh
@@ -10,7 +10,10 @@ sudo dpkg -i packages-microsoft-prod.deb
 sudo apt-get update
 sudo apt-get install apt-transport-https
 sudo apt-get update
+# This installs the full SDK allowing you to build from source. If you only want to run the compiled application, you
+# can install just the runtime by swapping the comment on the two lines below.
 sudo apt-get install dotnet-sdk-3.1
+# sudo apt-get install dotnet-runtime-3.1
 
 
 
