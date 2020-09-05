bc619d19265908dcd9b099d292269cbff06bba8d
Dávid El-Saig <david.el-saig@lombiq.com>
2020-09-05 20:51:33 +0200

    Fixing default endianness.

--- Hast.Abstractions/Hast.Vitis.Abstractions/Models/OpenClConfiguration.cs
+++ Hast.Abstractions/Hast.Vitis.Abstractions/Models/OpenClConfiguration.cs
@@ -1,12 +1,11 @@
 using Hast.Vitis.Abstractions.Interop.Enums.OpenCl;
-using Microsoft.Extensions.Configuration;
 
 namespace Hast.Vitis.Abstractions.Models
 {
     public class OpenClConfiguration : IOpenClConfiguration
     {
         public string BinaryFilePath { get; set; } = "HardwareFramework/rtl/xclbin/hastip.hw.xclbin";
-        public bool DeviceIsBigEndian { get; set; } = true;
+        public bool DeviceIsBigEndian { get; set; }
         public DeviceType DeviceType { get; set; } = DeviceType.Accelerator;
         public int HeaderCellCount { get; set; } = 4;
         public string VendorName { get; set; } = "Xilinx";
