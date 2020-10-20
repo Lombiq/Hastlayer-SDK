7b05c5f3bcc0aa66bf2a25f9c1e038a9a97d555a
Dávid El-Saig <david.el-saig@lombiq.com>
2020-10-20 21:33:01 +0200

    Hast.Console "vitis build" instruction

--- Hast.Console/Options/MainOptions.cs
+++ Hast.Console/Options/MainOptions.cs
@@ -7,6 +7,9 @@ namespace Hast.Console.Options
         [Option('i', "input", HelpText = "Path to the input file to transform.")]
         public string InputFilePath { get; set; }
 
+        [Option('o', "output", HelpText = "Path to the output file to transform.")]
+        public string OutputFilePath { get; set; }
+
         [Option('c', "list-commands", HelpText = "List the available sub-commands.")]
         public bool ListCommands { get; set; }
 
--- Hast.Console/Subcommands/VitisSubcommand.cs
+++ Hast.Console/Subcommands/VitisSubcommand.cs
@@ -1,8 +1,18 @@
 ﻿using CommandLine;
+using Hast.Common.Models;
 using Hast.Console.Attributes;
 using Hast.Console.Options;
+using Hast.Layer;
+using Hast.Synthesis.Abstractions;
+using Hast.Vitis.Abstractions.Services;
+using Hast.Xilinx.Abstractions;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
 using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using static System.Console;
 
 namespace Hast.Console.Subcommands
 {
@@ -11,6 +21,9 @@ namespace Hast.Console.Subcommands
     {
         private readonly MainOptions _mainOptions;
         private readonly string[] _rawArguments;
+
+        public ILogger<VitisHardwareImplementationComposerBuildProvider> BuildLogger { get; set; } =
+            new NullLogger<VitisHardwareImplementationComposerBuildProvider>();
 
         public VitisSubcommand(MainOptions mainOptions, string[] rawArguments)
         {
@@ -22,11 +35,52 @@ namespace Hast.Console.Subcommands
         {
         }
 
-        private void RunOptions(VitisOptions options)
+        private async Task RunOptionsAsync(VitisOptions options)
         {
             switch (Enum.Parse<Instruction>(options.Instruction, ignoreCase: true))
             {
                 case Instruction.Build:
+                    var vhdlPath = _mainOptions.InputFilePath;
+                    if (vhdlPath?.EndsWith(".vhdl") != true || !File.Exists(vhdlPath))
+                    {
+                        throw new ArgumentException("Please set the -i option to point to the main vhdl file! " +
+                                                    "(eg. ./HardwareFramework/rtl/src/IP/Hast_IP.vhd)");
+                    }
+
+                    var outputSet = !string.IsNullOrWhiteSpace(_mainOptions.OutputFilePath);
+                    if (outputSet && !_mainOptions.OutputFilePath.EndsWith(".xclbin"))
+                    {
+                        throw new ArgumentException("Please set the -o option to point to the desired location of the " +
+                                                    "xclbin file (eg. ./HardwareFramework/bin/Hastlayer.) or omit it!");
+                    }
+
+                    var manifest = new XilinxDeviceManifest();
+                    var transformationId = "Hastlayer";
+                    var context = new HardwareImplementationCompositionContext
+                    {
+                        DeviceManifest = manifest,
+                        Configuration = new HardwareGenerationConfiguration(
+                            manifest.Name,
+                            Path.GetFullPath("HardwareFramework")),
+                        HardwareDescription = new VhdlHardwareDescription
+                        {
+                            HardwareEntryPointNamesToMemberIdMappings = new Dictionary<string, int>(),
+                            TransformationId = transformationId,
+                            Warnings = Array.Empty<ITransformationWarning>(),
+                        },
+                    };
+                    var implementation = new HardwareImplementation
+                    {
+                        BinaryPath = outputSet
+                            ? _mainOptions.OutputFilePath
+                            : Path.Combine("VitisBuildOutput", transformationId + ".xclbin"),
+                    };
+
+
+                    await new VitisHardwareImplementationComposerBuildProvider(BuildLogger)
+                        .BuildAsync(context, implementation);
+
+                    WriteLine("Build Completed. Find files at: {0}", Path.GetFullPath(implementation.BinaryPath));
 
                     break;
                 default:
@@ -39,7 +93,7 @@ namespace Hast.Console.Subcommands
 
         public void Run() =>
             Parser.Default.ParseArguments<VitisOptions>(_rawArguments)
-                .WithParsed(RunOptions)
+                .WithParsed(options => RunOptionsAsync(options).Wait())
                 .WithNotParsed(HandleParseError);
 
         private enum Instruction
