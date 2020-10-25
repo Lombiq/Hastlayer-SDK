ecb15867d1f53af2d66c08468489245519d42493
Dávid El-Saig <david.el-saig@lombiq.com>
2020-10-25 23:42:32 +0100

    Added classes for parsing .rpt files.

---
+++ Hast.Abstractions/Hast.Vitis.Abstractions/Models/XilinxReport.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Hast.Vitis.Abstractions.Models
+{
+    public class XilinxReport
+    {
+        private const string HorizontalLine = "----";
+
+        public IDictionary<string, string> MetaData { get; } =
+            new Dictionary<string, string>(StringComparer.InvariantCultureIgnoreCase);
+        public IDictionary<string, XilinxReportSection> Sections { get; } =
+            new Dictionary<string, XilinxReportSection>(StringComparer.InvariantCultureIgnoreCase);
+
+
+        public static XilinxReport Parse(TextReader reader)
+        {
+            var report = new XilinxReport();
+
+            // Read the meta data. (block starts with a horizontal line)
+            ReadUntil(reader);
+            foreach (var metaLine in ReadWhile(reader, line => !line.StartsWith(HorizontalLine)))
+            {
+                if (!metaLine.Contains(':')) continue;
+                var parts = metaLine.TrimStart('|').Split(new[] { ':' }, 2);
+                report.MetaData[parts[0].Trim()] = parts[1].Trim();
+            }
+
+            for (var n = 1; ; n++)
+            {
+                // Scroll to section start with underlined header that starts with the chapter number.
+                var header = ReadUntil(reader, $"{n}. ")?.Split(new [] {'.'}, 2).Last().Trim();
+                if (header == null) break;
+                ReadUntil(reader);
+
+                report.Sections[header] = XilinxReportSection.Parse(reader);
+            }
+
+            return report;
+        }
+
+
+        public static string ReadUntil(TextReader reader, string start = HorizontalLine)
+        {
+            do
+            {
+                var line = reader.ReadLine();
+                if (line?.StartsWith(start) != false) return line;
+            } while (true);
+        }
+
+        public static IEnumerable<string> ReadWhile(
+            TextReader reader,
+            Func<string, bool> condition,
+            bool includeLast = false)
+        {
+            do
+            {
+                var line = reader.ReadLine();
+                if (line == null) yield break;
+
+                if (!condition(line))
+                {
+                    if (includeLast) yield return line;
+                    yield break;
+                }
+
+                yield return line;
+            } while (true);
+        }
+    }
+}
---
+++ Hast.Abstractions/Hast.Vitis.Abstractions/Models/XilinxReportSection.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using static Hast.Vitis.Abstractions.Models.XilinxReport;
+
+namespace Hast.Vitis.Abstractions.Models
+{
+    public class XilinxReportSection
+    {
+        private const string TableBorderLine = "+---";
+
+        private readonly string[][] _data;
+
+        public int Rows { get; }
+        public IReadOnlyList<string> Columns { get; }
+        public IReadOnlyList<string> Comments { get; }
+
+
+        public IDictionary<string, string> this[int rowIndex] =>
+            _data[rowIndex]
+                .Select((item, columnIndex) => new { Cell = item, ColumnIndex = columnIndex })
+                .ToDictionary(item => Columns[item.ColumnIndex], item => item.Cell);
+
+
+        public XilinxReportSection(string[][] data, IReadOnlyList<string> columns, IReadOnlyList<string> comments)
+        {
+            _data = data;
+            Rows = data.Length;
+            Columns = columns;
+            Comments = comments;
+        }
+
+
+        public static XilinxReportSection Parse(TextReader reader)
+        {
+            // Scroll to the table start. Each section always seems to have a table even if empty.
+            ReadUntil(reader, TableBorderLine);
+            var columnNames = reader.ReadLine()!.Trim('|').Split('|').Select(columnName => columnName.Trim())
+                .ToList();
+            ReadUntil(reader, TableBorderLine);
+
+            // Read each line into a table and then drop the bottom table border line.
+            var data = ReadWhile(reader, line => !line.StartsWith(TableBorderLine))
+                .Select(line => line.Trim('|')
+                    .Split('|')
+                    .Select(cell => cell.Trim())
+                    .ToArray())
+                .ToArray();
+
+            // Post-table comments start with an asterisk.
+            var comments = new List<string>();
+            while (reader.ReadLine() is { } line && line.StartsWith("*")) comments.Add(line);
+
+            return new XilinxReportSection(data, columnNames, comments);
+        }
+    }
+}
