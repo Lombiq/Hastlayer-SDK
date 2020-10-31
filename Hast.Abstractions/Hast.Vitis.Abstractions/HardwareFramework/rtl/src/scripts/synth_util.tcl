400eb34a7ca6c6957ea0bd01f24150f3a63f31ef
Dávid El-Saig <david.el-saig@lombiq.com>
2020-10-31 01:46:26 +0100

    Removed HardwareFramework from Hast.Vitis.Abstractions.

--- Hast.Abstractions/Hast.Vitis.Abstractions/HardwareFramework/rtl/src/scripts/synth_util.tcl
+++
@@ -1,6 +0,0 @@
-﻿set vhdlname [lindex $::argv 0]
-set rptname [lindex $::argv 1]
-
-read_vhdl "${vhdlname}"
-synth_design -top Hast_IP -part xcu250-figd2104-2L-e
-report_utilization -file "${rptname}"
