65e746ba22d3a08ecb8b53fc45d8c6bc002ed34f
Dávid El-Saig <david.el-saig@lombiq.com>
2021-01-11 00:14:01 +0100

    Added manifest providers.

---
+++ Hast.Abstractions/Hast.Xilinx.Abstractions/ManifestProviders/TrenzTE071504301CManifestProvider.cs
@@ -0,0 +1,9 @@
+﻿namespace Hast.Xilinx.Abstractions.ManifestProviders
+{
+    public class TrenzTE071504301CManifestProvider : ZynqManifestProviderBase
+    {
+        public const string DeviceName = "TE0715-04-30-1C";
+
+        public TrenzTE071504301CManifestProvider() => _deviceName = DeviceName;
+    }
+}
---
+++ Hast.Abstractions/Hast.Xilinx.Abstractions/ManifestProviders/ZynqManifestProviderBase.cs
@@ -0,0 +1,34 @@
+﻿using Hast.Layer;
+using Hast.Synthesis.Abstractions;
+using Hast.Xilinx.Abstractions.Helpers;
+using System.Text.RegularExpressions;
+using static Hast.Common.Constants.DataSize;
+using static Hast.Common.Constants.Frequency;
+
+namespace Hast.Xilinx.Abstractions.ManifestProviders
+{
+    public abstract class ZynqManifestProviderBase : IDeviceManifestProvider
+    {
+        protected string _deviceName;
+
+        private IDeviceManifest _deviceManifest;
+        public IDeviceManifest DeviceManifest =>
+            _deviceManifest ??= new XilinxDeviceManifest
+            {
+                Name = _deviceName,
+                ClockFrequencyHz = 80 * Mhz,
+                SupportedCommunicationChannelNames = new[] { Constants.VitisCommunicationChannelName },
+                AvailableMemoryBytes = 1 * GigaByte,
+                SupportsHbm = false,
+                SupportedPlatforms = new[]
+                {
+                    Regex.Replace(_deviceName.ToLower(), @"[^a-z0-9]+", "-"),
+                    "hw_platform",
+                },
+                ToolChainName = CommonToolChainNames.Vitis,
+            };
+
+        public void ConfigureMemory(MemoryConfiguration memory, IHardwareGenerationConfiguration hardwareGeneration) =>
+            MemoryConfigurationHelper.ConfigureMemoryForVitis(memory, hardwareGeneration);
+    }
+}
