18844d876e8ee3051afb15a0349d1e2c118ed7e3
Dávid El-Saig <david.el-saig@lombiq.com>
2021-04-08 22:22:17 +0200

    Revert "Revert "Migrate away from GDI+ to ImageSharp.""
    This reverts commit eebb3a9abfaa393a1b0cc1eefcbbec21d77e9ddb.

--- Samples/Hast.Samples.Consumer/SampleRunners/ImageProcessingAlgorithmsSampleRunner.cs
+++ Samples/Hast.Samples.Consumer/SampleRunners/ImageProcessingAlgorithmsSampleRunner.cs
@@ -1,35 +1,35 @@
 using Hast.Layer;
 using Hast.Samples.SampleAssembly;
-using System.Drawing;
-using System.Drawing.Imaging;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Formats.Bmp;
+using SixLabors.ImageSharp.PixelFormats;
 using System.Threading.Tasks;
 
 namespace Hast.Samples.Consumer.SampleRunners
 {
     internal class ImageProcessingAlgorithmsSampleRunner : ISampleRunner
     {
-        public void Configure(HardwareGenerationConfiguration configuration)
-        {
-            configuration.AddHardwareEntryPointType<ImageContrastModifier>();
-
+        public void Configure(HardwareGenerationConfiguration configuration) =>
+            configuration.AddHardwareEntryPointType<ImageContrastModifier>()
             // ImageFilter is not parallelized, so not including it not to take away FPGA resources from
             // ImageContrastModifier.
-            //configuration.AddHardwareEntryPointType<ImageFilter>();
-        }
+            // .AddHardwareEntryPointType<ImageFilter>()
+            ;
+
 
         public async Task Run(IHastlayer hastlayer, IHardwareRepresentation hardwareRepresentation, IProxyGenerationConfiguration configuration)
         {
             // In case you wish to test the sample with a larger file, the fpga.jpg file must be replaced. You can find
             // a 100 megapixel jpeg here: https://photographingspace.com/100-megapixel-moon/
-            using var bitmap = new Bitmap("fpga.jpg");
+            using var bitmap = await Image.LoadAsync<Rgba32>("fpga.jpg");
 
             var imageContrastModifier = await hastlayer
                 .GenerateProxy(hardwareRepresentation, new ImageContrastModifier(), configuration);
             var modifiedImage = imageContrastModifier.ChangeImageContrast(bitmap, -50, hastlayer, hardwareRepresentation.HardwareGenerationConfiguration);
-            modifiedImage.Save("contrast.bmp", ImageFormat.Bmp);
+            await modifiedImage.SaveAsync("contrast.bmp", new BmpEncoder());
 
             var sw = System.Diagnostics.Stopwatch.StartNew();
-            var cpuOutput = new ImageContrastModifier().ChangeImageContrast(bitmap, -50, hastlayer, hardwareRepresentation.HardwareGenerationConfiguration);
+            new ImageContrastModifier().ChangeImageContrast(bitmap, -50, hastlayer, hardwareRepresentation.HardwareGenerationConfiguration);
             sw.Stop();
             System.Console.WriteLine("On CPU it took " + sw.ElapsedMilliseconds + " ms.");
 
