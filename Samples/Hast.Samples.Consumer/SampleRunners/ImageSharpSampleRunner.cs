cd8940f4502464c6d1b3753d635e190d698a3ead
Márk Lukovics <mark.lukovics@lombiq.com>
2021-06-01 13:54:44 +0200

    Adding the basic sample from HAST-169

--- Samples/Hast.Samples.Consumer/SampleRunners/ImageSharpSampleRunner.cs
+++ Samples/Hast.Samples.Consumer/SampleRunners/ImageSharpSampleRunner.cs
@@ -1,5 +1,6 @@
 using Hast.Layer;
 using Hast.Samples.SampleAssembly;
+using Hastlayer_ImageSharp_PracticeDemo.Resize;
 using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.Processing;
 using System.Drawing.Imaging;
@@ -16,6 +17,15 @@ namespace Hast.Samples.Consumer.SampleRunners
 
         public async Task Run(IHastlayer hastlayer, IHardwareRepresentation hardwareRepresentation, IProxyGenerationConfiguration configuration)
         {
+            // Not accelerated by hastlayer
+            using var img = Image.Load("fpga.jpg");
+            var sw = System.Diagnostics.Stopwatch.StartNew();
+            img.Mutate(x => x.HastResize(img.Width/2, img.Height/2, System.Environment.ProcessorCount));
+            img.Save("resized_fpga.jpg");
+            sw.Stop();
+            System.Console.WriteLine($"On CPU it took {sw.ElapsedMilliseconds} ms");
+
+
             // In case you wish to test the sample with a larger file, the fpga.jpg file must be replaced. You can find
             // a 100 megapixel jpeg here: https://photographingspace.com/100-megapixel-moon/
             //using var image = new Bitmap("fpga.jpg");
@@ -24,9 +34,9 @@ namespace Hast.Samples.Consumer.SampleRunners
             var resizeImage = await hastlayer
                 .GenerateProxy(hardwareRepresentation, new ImageSharpSample(), configuration);
             var modifiedImage = resizeImage.HastResize(image);
-            modifiedImage.Save("resized_fpga.jpg");
+            modifiedImage.Save("resized_with_hastlayer_fpga.jpg");
 
-            var sw = System.Diagnostics.Stopwatch.StartNew();
+            sw.Restart();
             var cpuOutput = new ImageSharpSample().HastResize(image);
             sw.Stop();
             System.Console.WriteLine($"On CPU it took {sw.ElapsedMilliseconds} ms");
