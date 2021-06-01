9ebfcf6ef10ab82365b77992d040d330d09a5d3c
Márk Lukovics <mark.lukovics@lombiq.com>
2021-06-01 13:00:20 +0200

    Adding more comments

--- Samples/Hast.Samples.Consumer/SampleRunners/ImageSharpSampleRunner.cs
+++ Samples/Hast.Samples.Consumer/SampleRunners/ImageSharpSampleRunner.cs
@@ -1,9 +1,10 @@
 using Hast.Layer;
 using Hast.Samples.SampleAssembly;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Processing;
 using System.Drawing;
 using System.Drawing.Imaging;
 using System.Threading.Tasks;
-
 
 namespace Hast.Samples.Consumer.SampleRunners
 {
@@ -16,15 +17,19 @@ namespace Hast.Samples.Consumer.SampleRunners
 
         public async Task Run(IHastlayer hastlayer, IHardwareRepresentation hardwareRepresentation, IProxyGenerationConfiguration configuration)
         {
+            // In case you wish to test the sample with a larger file, the fpga.jpg file must be replaced. You can find
+            // a 100 megapixel jpeg here: https://photographingspace.com/100-megapixel-moon/
             using var image = new Bitmap("fpga.jpg"); // Change to IS Image later?
+            // using var image = SixLabors.ImageSharp.Image.Load("fpga.jpg");
 
             var resizeImage = await hastlayer
                 .GenerateProxy(hardwareRepresentation, new ImageSharpSample(), configuration);
-            // var modifiedImage = resizeImage.HastResize()
+            // TODO: create method in ImageSharpSample.cs
+            // var modifiedImage = resizeImage.HastResize();
             // modifiedImage.Save('resized.png', ImageFormat.Png);
 
             var sw = System.Diagnostics.Stopwatch.StartNew();
-            // var cpuOutput = new ImageSharpResize().Do Stuff
+            // var cpuOutput = new ImageSharpSample().HastResize();
             sw.Stop();
             System.Console.WriteLine($"On CPU it took {sw.ElapsedMilliseconds} ms");
         }
