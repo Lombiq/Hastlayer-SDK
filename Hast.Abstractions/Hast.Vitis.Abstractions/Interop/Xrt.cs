7d0f73416cecb3e90ca139785fa3ed49517d0ac7
Dávid El-Saig <david.el-saig@lombiq.com>
2021-06-06 21:49:13 +0200

    Fix XilinxMemoryExtension.

--- Hast.Abstractions/Hast.Vitis.Abstractions/Interop/Xrt.cs
+++ Hast.Abstractions/Hast.Vitis.Abstractions/Interop/Xrt.cs
@@ -1,5 +1,6 @@
 using System;
 using Hast.Vitis.Abstractions.Interop.Enums.OpenCl;
+using System.Diagnostics.CodeAnalysis;
 
 namespace Hast.Vitis.Abstractions.Interop
 {
@@ -11,18 +12,35 @@ namespace Hast.Vitis.Abstractions.Interop
     /// Legacy layout is used since that's the one in the examples
     /// <see href="https://github.com/Xilinx/Vitis_Accel_Examples/blob/master/host/hbm_bandwidth/src/host.cpp">here</see>.</para>
     /// </remarks>
-    public struct XilinxMemoryExtension
+    public struct XilinxMemoryExtension : IEquatable<XilinxMemoryExtension>
     {
         public ulong Flags { get; set; }
+
+        [SuppressMessage(
+            "Naming",
+            "CA1720:Identifier contains type name",
+            Justification = "Intentional. It refers to the \"obj\" field in the external structure.")]
         public IntPtr Object { get; set; }
         public IntPtr Parameters { get; set; }
 
         public static XilinxMemoryExtension Create(IntPtr data, ulong bank) =>
-            new XilinxMemoryExtension
+            new ()
             {
                 Flags = bank | (ulong)MemoryFlags.ExtensionXilinxTopology,
                 Object = data,
                 Parameters = IntPtr.Zero,
             };
+
+        public override bool Equals(object obj) => obj is XilinxMemoryExtension extension && Equals(extension);
+
+        public bool Equals(XilinxMemoryExtension other) =>
+            Flags == other.Flags &&
+            Object == other.Object &&
+            Parameters == other.Parameters;
+
+        public override int GetHashCode() => HashCode.Combine(Flags, Object, Parameters);
+
+        public static bool operator ==(XilinxMemoryExtension left, XilinxMemoryExtension right) => left.Equals(right);
+        public static bool operator !=(XilinxMemoryExtension left, XilinxMemoryExtension right) => !left.Equals(right);
     }
 }
