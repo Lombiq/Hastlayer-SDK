bc92ef86a6e97323383ff344f33bc151ad282044
Dávid El-Saig <david.el-saig@lombiq.com>
2021-10-01 07:00:14 +0200

    sh improvements.

--- Hast.Abstractions/Hast.Vitis.Abstractions/Docs/Attachments/zynq-benchmark.dot.sh
+++ Hast.Abstractions/Hast.Vitis.Abstractions/Docs/Attachments/zynq-benchmark.dot.sh
@@ -8,13 +8,14 @@ function title()
   repeat $((${#1} + 4)) '*'
   echo "* $1 *"
   repeat $((${#1} + 4)) '*'
+  echo
 }
 
 function cls() { clear; echo -e '\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n'; clear; }
 
 # Initialize .NET and other environment settings.
 
-export DOTNET_ROOT=/media/sd-mmcblk0p1/dotnet-sdk-5*-linux-arm/
+export DOTNET_ROOT="$(realpath /media/sd-mmcblk0p1/dotnet-sdk-5*-linux-arm/)"
 export PATH=$PATH:$DOTNET_ROOT
 export DOTNET_SYSTEM_GLOBALIZATION_INVARIANT=1
 export DOTNET_CLI_TELEMETRY_OPTOUT=1
@@ -23,29 +24,35 @@ export XILINX_XRT=/usr
 export BENCHMARK_RUN_DOT_READY=1
 [ -f /usr/lib/libOpenCL.so ] || ln -s /usr/lib/libOpenCL.so.1 /usr/lib/libOpenCL.so
 [ -f /lib/libdl.so ] ||ln -s /lib/libdl.so.2 /lib/libdl.so
-[ -d /media/sd-mmcblk0p1/Hast.Samples.Consumer/HardwareFramework/bin ] || mkdir /media/sd-mmcblk0p1/Hast.Samples.Consumer/HardwareFramework/bin
 
 # Define program
-
-ConsumerPath="/media/sd-mmcblk0p1/Hast.Samples.Consumer"
 
 function run-benchmark-inner()
 {
   XclbinFileName="$1"
   shift
 
-  Label="$(cat "$XclbinFileName.name")"
+  DllDirectoryPath="/media/sd-mmcblk0p1/Hast.Samples.Consumer"
+  DllFileName="Hast.Samples.Consumer.dll"
+  if [[ $1 == *.dll ]]; then
+      DllFileName="$(basename "$1")"
+      DllDirectoryPath="$(dirname "$1")"
+      shift
+  fi
 
-  title "$XclbinFileName - $Label - $(date)"
+  Label="$XclbinFileName"
+  [ -f "$XclbinFileName.name" ] && Label="$(cat "$XclbinFileName.name")"
+
+  title "$XclbinFileName - $Label"
 
   echo fpgautil -b "$(echo $XclbinFileName | sed 's/\.xclbin$/.bit.bin/')"
-  time fpgautil -b "$(echo $XclbinFileName | sed 's/\.xclbin$/.bit.bin/')"
+  fpgautil -b "$(echo $XclbinFileName | sed 's/\.xclbin$/.bit.bin/')"
 
   ProjectPath="$PWD"
-  pushd "$ConsumerPath"
-    echo dotnet Hast.Samples.Consumer.dll -device "TE0715-04-30-1C" -sample $(echo "$Label" | sed 's/\s.*//') "$@" -bin "$ProjectPath/$XclbinFileName"
-    time dotnet Hast.Samples.Consumer.dll -device "TE0715-04-30-1C" -sample $(echo "$Label" | sed 's/\s.*//') "$@" -bin "$ProjectPath/$XclbinFileName"
-  popd
+  pushd "$DllDirectoryPath" > /dev/null
+    echo dotnet "$DllFileName" -device "TE0715-04-30-1C" -sample $(echo "$Label" | sed 's/\s.*//') "$@" -bin "$ProjectPath/$XclbinFileName"
+    time dotnet "$DllFileName" -device "TE0715-04-30-1C" -sample $(echo "$Label" | sed 's/\s.*//') "$@" -bin "$ProjectPath/$XclbinFileName"
+  popd > /dev/null
 
   echo "FCLK0 = $(cat /sys/devices/soc0/fclk0/set_rate)Hz"
 }
@@ -53,16 +60,27 @@ function run-benchmark-inner()
 function run-benchmark()
 {
   if [ $# -eq 0 ]; then
-    echo "USAGE: $0 'filename.xclbin'"
+    echo "USAGE: $0 'filename.xclbin [executable.dll]'"
     echo "    The filename must be a relative path to the current directory."
     return
   fi
 
-  XclbinFileName="$1"
+  FullXclbinFilePath="$(realpath "$1")"
+  XclbinFileName="$(basename "$FullXclbinFilePath")"
+  XclbinDirectoryPath="$(dirname "$FullXclbinFilePath")"
   shift
 
-  [ -f "$XclbinFileName.log" ] && rm "$XclbinFileName.log"
-  run-benchmark-inner "$XclbinFileName" "$@" | tee "$XclbinFileName.log"
+
+  if [[ $1 == *.dll ]]; then
+      FullDllFilePath="$(realpath "$1")"
+      shift
+      set -- "$FullDllFilePath" "$@"
+  fi
+
+  pushd "$XclbinDirectoryPath" > /dev/null
+    [ -f "$XclbinFileName.log" ] && rm "$XclbinFileName.log"
+    run-benchmark-inner "$XclbinFileName" "$@" | tee "$XclbinFileName.log"
+  popd > /dev/null
 }
 
 function run-and-verify()
