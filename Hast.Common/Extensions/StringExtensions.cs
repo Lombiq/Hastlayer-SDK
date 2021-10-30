be19839edc7c304c872a29536bea66742e729317
Dávid El-Saig <david.el-saig@lombiq.com>
2021-10-30 02:24:26 +0200

    Additional string extension.

--- Hast.Common/Extensions/StringExtensions.cs
+++ Hast.Common/Extensions/StringExtensions.cs
@@ -7,13 +7,16 @@ namespace System
         /// <summary>
         /// Turns a camelCase or PascalCase token into snake_Case.
         /// </summary>
-        /// <param name="str">The input in camelCase or PascalCase.</param>
+        /// <param name="input">The input in camelCase or PascalCase.</param>
         /// <returns>
         /// The input converted to snake_Case. It doesn't alter case so you can call either ToUpper or ToLower without
         /// any additional penalties.
         /// </returns>
         /// <remarks>Source: https://stackoverflow.com/a/18781533 </remarks>
         public static string ToSnakeCase(this string input) =>
-            string.Concat(input.Select((x, i) => i > 0 && char.IsUpper(x) ? "_" + x.ToString() : x.ToString()));
+            string.Concat(input.Select((x, i) => i > 0 && char.IsUpper(x) ? "_" + x : x.ToString()));
+
+        public static string OrIfEmpty(this string text, string alternative) =>
+            string.IsNullOrWhiteSpace(text) ? alternative : text;
     }
 }
