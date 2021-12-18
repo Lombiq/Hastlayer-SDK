76788b0bca7e6ccd4302a1199ecc1c9a8ca06be0
Dávid El-Saig <david.el-saig@lombiq.com>
2021-12-18 03:42:24 +0100

    Remove unnecessary casts.

--- Hast.Communication/Services/DevicePoolManager.cs
+++ Hast.Communication/Services/DevicePoolManager.cs
@@ -1,4 +1,4 @@
-using Hast.Communication.Models;
+﻿using Hast.Communication.Models;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
@@ -57,7 +57,7 @@ namespace Hast.Communication.Services
 
                 if (firstAvailableDevice != null)
                 {
-                    _logger.LogDebug("Found an available device with the identifier {0}.", (object)firstAvailableDevice.Identifier);
+                    _logger.LogDebug("Found an available device with the identifier {0}.", firstAvailableDevice.Identifier);
 
                     firstAvailableDevice.IsBusy = true;
 
@@ -79,7 +79,7 @@ namespace Hast.Communication.Services
                             {
                                 _logger.LogDebug(
                                     "No device reservation requests are in the queue so freeing up the device with the ID {0}.",
-                                    (object)thisReservedDevice.Identifier);
+                                    thisReservedDevice.Identifier);
 
                                 _devicePool[thisReservedDevice.Identifier].IsBusy = false;
                             }
