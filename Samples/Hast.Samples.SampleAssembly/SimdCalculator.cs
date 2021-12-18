2182c479a5986526e914e293769742a031af21e8
Dávid El-Saig <david.el-saig@lombiq.com>
2021-12-18 22:21:34 +0100

    Fix SimdCalculator's failing transformation.

--- Samples/Hast.Samples.SampleAssembly/SimdCalculator.cs
+++ Samples/Hast.Samples.SampleAssembly/SimdCalculator.cs
@@ -1,4 +1,4 @@
-using Hast.Common.Numerics;
+﻿using Hast.Common.Numerics;
 using Hast.Layer;
 using Hast.Transformer.Abstractions.SimpleMemory;
 using System;
@@ -15,16 +15,15 @@ namespace Hast.Samples.SampleAssembly
     }
 
     /// <summary>
-    /// Sample to showcase SIMD (Simple Instruction Multiple Data) processing usage, i.e. operations executed in
-    /// parallel on multiple elements of vectors. Also see <c>SimdCalculatorSampleRunner</c> on what to configure to
-    /// make this work.
+    /// Sample to showcase SIMD (Simple Instruction Multiple Data) processing usage, i.e. operations executed in parallel
+    /// on multiple elements of vectors. Also see <c>SimdCalculatorSampleRunner</c> on what to configure to make this
+    /// work.
     /// </summary>
     /// <remarks>
     /// <para>
-    /// <c>System.Numerics.Vector</c>s (including the NuGet package version of it
-    /// (http://www.nuget.org/packages/System.Numerics.Vectors) could be used for SIMD processing on x64 systems.
-    /// However <see cref="Vector{T}"/> can only contain as many elements that can fit into the processor's SIMD
-    /// register and thus is quite inconvenient to use. So using a custom implementation.
+    /// <c>System.Numerics.Vectors</c> could be used for SIMD processing on x64 systems. However <see cref="Vector{T}"/>
+    /// can only contain that many elements that can fit into the processor's SIMD register and thus is quite
+    /// inconvenient to use. So using a custom implementation.
     /// </para>
     /// </remarks>
     public class SimdCalculator
@@ -42,35 +41,11 @@ namespace Hast.Samples.SampleAssembly
 
         public virtual void AddVectors(SimpleMemory memory) => RunSimdOperation(memory, SimdOperation.Add);
 
-        public int[] AddVectors(int[] vector1, int[] vector2, IHastlayer hastlayer = null, IHardwareGenerationConfiguration configuration = null) =>
-            RunSimdOperation(vector1, vector2, memory => AddVectors(memory), hastlayer, configuration);
-
         public virtual void SubtractVectors(SimpleMemory memory) => RunSimdOperation(memory, SimdOperation.Subtract);
-
-        public int[] SubtractVectors(
-            int[] vector1,
-            int[] vector2,
-            IHastlayer hastlayer = null,
-            IHardwareGenerationConfiguration configuration = null) =>
-            RunSimdOperation(vector1, vector2, memory => SubtractVectors(memory), hastlayer, configuration);
 
         public virtual void MultiplyVectors(SimpleMemory memory) => RunSimdOperation(memory, SimdOperation.Multiply);
 
-        public int[] MultiplyVectors(
-            int[] vector1,
-            int[] vector2,
-            IHastlayer hastlayer = null,
-            IHardwareGenerationConfiguration configuration = null) =>
-            RunSimdOperation(vector1, vector2, memory => MultiplyVectors(memory), hastlayer, configuration);
-
         public virtual void DivideVectors(SimpleMemory memory) => RunSimdOperation(memory, SimdOperation.Divide);
-
-        public int[] DivideVectors(
-            int[] vector1,
-            int[] vector2,
-            IHastlayer hastlayer = null,
-            IHardwareGenerationConfiguration configuration = null) =>
-            RunSimdOperation(vector1, vector2, memory => DivideVectors(memory), hastlayer, configuration);
 
         private void RunSimdOperation(SimpleMemory memory, SimdOperation operation)
         {
@@ -83,6 +58,11 @@ namespace Hast.Samples.SampleAssembly
                 var vector1 = new int[MaxDegreeOfParallelism];
                 var vector2 = new int[MaxDegreeOfParallelism];
 
+                // It is necessary so the IArraySizeHolder knows the allocation size of the variable.
+#pragma warning disable S1854 // Unused assignments should be removed
+                var resultVector = new int[MaxDegreeOfParallelism];
+#pragma warning restore S1854 // Unused assignments should be removed
+
                 for (int m = 0; m < MaxDegreeOfParallelism; m++)
                 {
                     vector1[m] = memory.ReadInt32(VectorElementsStartInt32Index + i + m);
@@ -93,14 +73,27 @@ namespace Hast.Samples.SampleAssembly
                     vector2[m] = memory.ReadInt32(VectorElementsStartInt32Index + i + m + elementCount);
                 }
 
-                int[] resultVector = operation switch
+                // This prevents the code from turning into a switch expression during decompilation. Those aren't
+                // supported yet.
+#pragma warning disable IDE0010 // Add missing cases
+#pragma warning disable S131 // "switch/Select" statements should contain a "default/Case Else" clauses
+                switch (operation)
                 {
-                    SimdOperation.Add => SimdOperations.AddVectors(vector1, vector2, MaxDegreeOfParallelism),
-                    SimdOperation.Subtract => SimdOperations.SubtractVectors(vector1, vector2, MaxDegreeOfParallelism),
-                    SimdOperation.Multiply => SimdOperations.MultiplyVectors(vector1, vector2, MaxDegreeOfParallelism),
-                    SimdOperation.Divide => SimdOperations.DivideVectors(vector1, vector2, MaxDegreeOfParallelism),
-                    _ => new int[MaxDegreeOfParallelism],
-                };
+                    case SimdOperation.Add:
+                        resultVector = SimdOperations.AddVectors(vector1, vector2, MaxDegreeOfParallelism);
+                        break;
+                    case SimdOperation.Subtract:
+                        resultVector = SimdOperations.SubtractVectors(vector1, vector2, MaxDegreeOfParallelism);
+                        break;
+                    case SimdOperation.Multiply:
+                        resultVector = SimdOperations.MultiplyVectors(vector1, vector2, MaxDegreeOfParallelism);
+                        break;
+                    case SimdOperation.Divide:
+                        resultVector = SimdOperations.DivideVectors(vector1, vector2, MaxDegreeOfParallelism);
+                        break;
+                }
+#pragma warning restore S131 // "switch/Select" statements should contain a "default/Case Else" clauses
+#pragma warning restore IDE0010 // Add missing cases
 
                 for (int m = 0; m < MaxDegreeOfParallelism; m++)
                 {
@@ -111,12 +104,19 @@ namespace Hast.Samples.SampleAssembly
             }
         }
 
-        private int[] RunSimdOperation(
-            int[] vector1,
-            int[] vector2,
-            Action<SimpleMemory> operation,
-            IHastlayer hastlayer = null,
-            IHardwareGenerationConfiguration configuration = null)
+        public int[] AddVectors(int[] vector1, int[] vector2, IHastlayer hastlayer = null, IHardwareGenerationConfiguration configuration = null) =>
+            RunSimdOperation(vector1, vector2, memory => AddVectors(memory), hastlayer, configuration);
+
+        public int[] SubtractVectors(int[] vector1, int[] vector2, IHastlayer hastlayer = null, IHardwareGenerationConfiguration configuration = null) =>
+            RunSimdOperation(vector1, vector2, memory => SubtractVectors(memory), hastlayer, configuration);
+
+        public int[] MultiplyVectors(int[] vector1, int[] vector2, IHastlayer hastlayer = null, IHardwareGenerationConfiguration configuration = null) =>
+            RunSimdOperation(vector1, vector2, memory => MultiplyVectors(memory), hastlayer, configuration);
+
+        public int[] DivideVectors(int[] vector1, int[] vector2, IHastlayer hastlayer = null, IHardwareGenerationConfiguration configuration = null) =>
+            RunSimdOperation(vector1, vector2, memory => DivideVectors(memory), hastlayer, configuration);
+
+        private int[] RunSimdOperation(int[] vector1, int[] vector2, Action<SimpleMemory> operation, IHastlayer hastlayer = null, IHardwareGenerationConfiguration configuration = null)
         {
             SimdOperations.ThrowIfVectorsNotEquallyLong(vector1, vector2);
 
