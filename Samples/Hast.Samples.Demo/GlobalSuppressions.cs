35856de7ab755927ea930169e7943236f44f0c5f
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2022-12-13 18:39:52 +0100

    Fixing code analysis violations

--- Samples/Hast.Samples.Demo/GlobalSuppressions.cs
+++ Samples/Hast.Samples.Demo/GlobalSuppressions.cs
@@ -8,17 +8,14 @@ using System.Diagnostics.CodeAnalysis;
     "Minor Code Smell",
     "S1481:Unused local variables should be removed",
     Justification = "Information in the variable name.",
-    Scope = "member",
-    Target = "~M:Hast.Samples.Demo.Program.Main~System.Threading.Tasks.Task")]
+    Scope = "module")]
 [assembly: SuppressMessage(
     "Globalization",
     "CA1303:Do not pass literals as localized parameters",
     Justification = "This app is not localized.",
-    Scope = "member",
-    Target = "~M:Hast.Samples.Demo.Program.Main~System.Threading.Tasks.Task")]
+    Scope = "module")]
 [assembly: SuppressMessage(
     "StyleCop.CSharp.ReadabilityRules",
     "SA1123:Do not place regions within elements",
     Justification = "Regions help sectioning the demo making it more suitable for presentation.",
-    Scope = "member",
-    Target = "~M:Hast.Samples.Demo.Program.Main~System.Threading.Tasks.Task")]
+    Scope = "module")]
