57fc57920137e9ce8e24d6825058426f1717223f
Dávid El-Saig <david.el-saig@lombiq.com>
2023-07-21 11:12:56 +0200

    fix timing report file name for Azure Alveo U250 driver

--- src/Hastlayer/Hast.Xilinx/Drivers/AzureAlveoU250Driver.cs
+++ src/Hastlayer/Hast.Xilinx/Drivers/AzureAlveoU250Driver.cs
@@ -10,6 +10,8 @@ public class AzureAlveoU250Driver : VitisDeviceDriverBase
     public override string PlatformName => "xilinx_u250_gen3x16_xdma_2_1_202010_1"; // Needs a very specific version.
     public override uint ClockFrequencyMhz => 300;
 
+    protected override string TimingReportFileName => nameof(AlveoU250Driver);
+
     public AzureAlveoU250Driver(ITimingReportParser timingReportParser)
         : base(timingReportParser)
     { }
